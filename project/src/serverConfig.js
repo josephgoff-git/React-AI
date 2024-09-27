2776eb2af6ed59f09e5ba6a4a974103cf6728fd8
JosephGoff <joeygoff13@gmail.com>
2024-09-27 15:05:44 -0400

    making new changes now

--- project/src/serverConfig.js
+++ project/src/serverConfig.js
@@ -1,6 +1,6 @@
 export const blob_base_url = "https://aws1-bucket1-jg.s3.amazonaws.com/reactaiplayground/"
 
-let local = false
+let local = true
 let server = "http://localhost:3001"
 // if (!local) {server = "https://reactaiplayground.azurewebsites.net"}
 if (!local) {server = "https://store.expertsoncall.online"}

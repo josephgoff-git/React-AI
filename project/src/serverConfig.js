90f324021b8ac97130bfb7de70619c9e31f1786f
JosephGoff <joeygoff13@gmail.com>
2024-11-13 19:59:48 -0500

    now using railway

--- project/src/serverConfig.js
+++ project/src/serverConfig.js
@@ -3,6 +3,7 @@ export const blob_base_url = "https://aws1-bucket1-jg.s3.amazonaws.com/reactaipl
 let local = false
 let server = "http://localhost:3001"
 // if (!local) {server = "https://reactaiplayground.azurewebsites.net"}
-if (!local) {server = "https://store.expertsoncall.online"}
+// if (!local) {server = "https://store.expertsoncall.online"} 
+if (!local) {server = "https://combined-project-server.up.railway.app"}
 
 export default server
